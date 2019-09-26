642c25297f240f6aa696d67f70bb5bfb457584dc
MER IAN <Your Name>
2019-09-26 17:19:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+print("MER")
+print('o----')
+print(' ||||')
+print('*'*10)
+print('<3'*2)
+print('$$'*5)
+print('3---3'*4)
+print('_@_'*3)
+
+##
+price = 10
+price = 20
+print(price)
+rating = 4.9
+print(rating)
+name = 'Mer'
+name ='Cabbage'
+print(name)
+is_published = False
+print(is_published)
+
+##
+name = 'John Smith'
+age = 20
+patient_type_new = True
+
